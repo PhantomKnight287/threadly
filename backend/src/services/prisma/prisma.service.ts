aed99a367701aa6c945605715b67c8b771489497
Gurpal Singh <gillgurpal699@gmail.com>
2023-07-22 23:14:15 +0530

    [backend] create prisma service

---
+++ backend/src/services/prisma/prisma.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+  constructor() {
+    super({
+      log: ['error', 'info', 'query', 'warn'],
+      errorFormat: 'pretty',
+    });
+  }
+}
