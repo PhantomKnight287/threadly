f2370f9871787cc038f5d4e0865a4b58d4b8b7ef
Gurpal Singh <gillgurpal699@gmail.com>
2023-07-24 00:03:27 +0530

    [mobile] create hook to set navigation options

---
+++ threadly/src/hooks/useSetOptions.tsx
@@ -0,0 +1,27 @@
+import { Stack, useNavigation } from "expo-router";
+import { ComponentProps, DependencyList, useLayoutEffect } from "react";
+import { DARK_COLOR } from "@/constants";
+import { WorkSans } from "@/constants/fonts";
+
+export default function useSetOptions(
+  props: ComponentProps<typeof Stack>["screenOptions"],
+  deps?: DependencyList
+) {
+  const { setOptions } = useNavigation();
+  useLayoutEffect(() => {
+    setOptions({
+      headerTitleAlign: "center",
+      headerTitleStyle: {
+        fontFamily: WorkSans.WorkSans_600SemiBold,
+      },
+      headerShadowVisible: false,
+      headerLargeTitleShadowVisible: false,
+      headerStyle: {
+        backgroundColor: DARK_COLOR,
+      },
+      headerTintColor: "white",
+      headerBackTitleVisible: false,
+      ...props,
+    });
+  }, deps);
+}
