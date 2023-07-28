1b6bd303e248b49b4e2297a603aa9bdb21a298fa
Gurpal Singh <gillgurpal699@gmail.com>
2023-07-28 20:23:00 +0530

    [mobile] make size of `GradientLogo` dynamic

--- threadly/src/components/shared/gradient.logo.tsx
+++ threadly/src/components/shared/gradient.logo.tsx
@@ -3,14 +3,18 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { Octicons } from '@expo/vector-icons';
 import MaskedView from '@react-native-masked-view/masked-view';
 
-function GradientLogo() {
+type GradientLogoProps = {
+    size?: number;
+};
+
+function GradientLogo({ size = 70 }: GradientLogoProps) {
     return (
         <MaskedView
-            maskElement={<Octicons name="mention" size={70} color="white" />}
+            maskElement={<Octicons name="mention" size={size} color="white" />}
         >
             <LinearGradient
                 colors={['#ffdd55', '#ff543e', '#d9408a', '#6f15fc']}
-                style={{ height: 70, width: 70 }}
+                style={{ height: size, width: size }}
                 start={{ x: 0, y: 0 }}
                 end={{ x: 1, y: 0 }}
             />
