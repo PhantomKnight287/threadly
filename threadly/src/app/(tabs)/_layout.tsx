b1ae0f3c0720b9d39e4188684ea891b12bc665e7
Gurpal Singh <gillgurpal699@gmail.com>
2023-07-28 20:22:12 +0530

    [mobile] create `TabsLayout`

---
+++ threadly/src/app/(tabs)/_layout.tsx
@@ -0,0 +1,109 @@
+import { Redirect, Tabs } from 'expo-router';
+
+import GradientLogo from '@/components/shared/gradient.logo';
+import useUser from '@/state/user';
+import { AntDesign, Feather, Ionicons } from '@expo/vector-icons';
+
+export default function TabsLayout() {
+    const { user } = useUser();
+    if (!user) return <Redirect href={'/auth/login'} />;
+    return (
+        <Tabs
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#0d0d0d',
+                },
+                headerShadowVisible: false,
+                headerTitleAlign: 'center',
+                headerTitle() {
+                    return <GradientLogo size={30} />;
+                },
+                tabBarStyle: {
+                    backgroundColor: '#0d0d0d',
+                    shadowColor: 'transparent',
+                    borderTopWidth: 0,
+                },
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: '',
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <Ionicons
+                                name={focused ? 'home' : 'home-outline'}
+                                size={32}
+                                color={'white'}
+                            />
+                        );
+                    },
+                }}
+            />
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: '',
+                    tabBarIcon: () => {
+                        return (
+                            <Feather
+                                name={'search'}
+                                size={32}
+                                color={'white'}
+                            />
+                        );
+                    },
+                }}
+            />
+            <Tabs.Screen
+                name="create"
+                options={{
+                    title: '',
+                    tabBarIcon: () => {
+                        return (
+                            <Ionicons
+                                name={'ios-create'}
+                                size={32}
+                                color={'white'}
+                            />
+                        );
+                    },
+                }}
+            />
+            <Tabs.Screen
+                name="notifications"
+                options={{
+                    title: '',
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <AntDesign
+                                name={focused ? 'heart' : 'hearto'}
+                                size={28}
+                                color={'white'}
+                            />
+                        );
+                    },
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: '',
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <Ionicons
+                                name={
+                                    focused
+                                        ? 'ios-person'
+                                        : 'ios-person-outline'
+                                }
+                                size={28}
+                                color={'white'}
+                            />
+                        );
+                    },
+                }}
+            />
+        </Tabs>
+    );
+}
--- threadly/src/app/index.tsx
+++ threadly/src/app/index.tsx
@@ -3,10 +3,14 @@ import { StyleSheet } from 'react-native';
 
 import Text from '@/components/shared/text';
 import View from '@/components/shared/view';
+import useHydrateUser from '@/hooks/useHydrateUser';
 import useSetOptions from '@/hooks/useSetOptions';
 
 export default function Page() {
     useSetOptions({});
+    useHydrateUser({
+        redirectIfNoToken: true,
+    });
     return (
         <View className={'bg-dark flex-1 items-center p-6'}>
             <View style={styles.main}>
@@ -19,6 +23,16 @@ export default function Page() {
                 <Link href={'/auth/register'}>
                     <Text className="text-2xl font-bold text-center text-blue-500">
                         Register
+                    </Text>
+                </Link>
+                <Link href={'/auth/login'}>
+                    <Text className="text-2xl font-bold text-center text-blue-500">
+                        Login
+                    </Text>
+                </Link>
+                <Link href="(tabs)">
+                    <Text>
+                        Hello
                     </Text>
                 </Link>
             </View>
