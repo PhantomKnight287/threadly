a1dee7e8552f675f20006f34f0ee70192d0e38cb
Gurpal Singh <gillgurpal699@gmail.com>
2023-07-28 20:22:24 +0530

    [mobile] create base screens

---
+++ threadly/src/app/(tabs)/create.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Text from '@/components/shared/text';
+import View from '@/components/shared/view';
+
+export default function Create() {
+    return (
+        <View>
+            <Text>Create</Text>
+        </View>
+    );
+}
---
+++ threadly/src/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Text from '@/components/shared/text';
+import View from '@/components/shared/view';
+
+export default function Base() {
+    return (
+        <View className="flex-1">
+            <Text>Base</Text>
+        </View>
+    );
+}
