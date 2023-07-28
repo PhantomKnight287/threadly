ee67577ccb88d0d5e8264cf5da214e78034e73b1
Gurpal Singh <gillgurpal699@gmail.com>
2023-07-28 23:46:03 +0530

    [app] create new constant

--- threadly/src/constants/index.ts
+++ threadly/src/constants/index.ts
@@ -2,3 +2,5 @@ export const DARK_COLOR = '#0d0d0d';
 export const LIGHT_COLOR = '#f5f5f5';
 export const BRAND_COLOR = '#0070fa';
 export const API_URL = 'http://10.0.2.2:3000';
+export const IMAGE_PLACEHOLDER =
+    '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
